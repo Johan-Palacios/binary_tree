c616eab1142839eda7d455f07d8836491f1abc14
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-10-24 18:08:43 -0600

    Delete Extra code

--- core/binary_tree.py
+++ core/binary_tree.py
@@ -1,28 +1,3 @@
-class Stack(object):
-    def __init__(self):
-        self.items = []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        if not self.is_empty():
-            return self.items.pop()
-
-    def is_empty(self):
-        return len(self.items) == 0
-
-    def peek(self):
-        if not self.is_empty():
-            return self.items[-1]
-
-    def __len__(self):
-        return self.size()
-
-    def size(self):
-        return len(self.items)
-
-
 class Queue(object):
     def __init__(self):
         self.items = []
